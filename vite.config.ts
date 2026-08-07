88d150dee85c93cb5699d74c7b9182a291814703
Amansingh2024 <amankumarsiitcse18@gmail.com>
2026-08-06 23:17:09 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aman-singh-rajput-portfollio/', // repository name
+  base: '/index.html/', // repository name
 })
